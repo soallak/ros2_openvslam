b6054dbb16443b05da3933086be1f3102ac7541e
Saad Ouassil Allak <sw.allak@gmail.com>
2022-03-22 19:15:44 +0100

    feat: add tracepoints

--- src/stereo.cpp
+++ src/stereo.cpp
@@ -1,8 +1,16 @@
 #include "stereo.hpp"
 
+#include <chrono>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <string>
+
+// clang-format off
+// todo: make this conditional at compile time
+#define LTTNG_UST_TRACEPOINT_DEFINE
+#define LTTNG_UST_TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#include <slam_tracepoint_provider/tracepoint.hpp>
+// clang-format on
 
 namespace openvslam_ros {
 
@@ -27,15 +35,20 @@ stereo::stereo(const std::shared_ptr<openvslam::config>& cfg,
 
 void stereo::callback(const sensor_msgs::msg::Image::ConstSharedPtr& left,
                       const sensor_msgs::msg::Image::ConstSharedPtr& right) {
+  TP_CALLBACK_ENTER(node_, node_->now(), __func__);
   auto leftcv = cv_bridge::toCvShare(left)->image;
   auto rightcv = cv_bridge::toCvShare(right)->image;
 
   const rclcpp::Time tp_1 = node_->now();
   // input the current frame and estimate the camera pose
+
   auto cam_pose_wc = SLAM_.feed_stereo_frame(
       leftcv, rightcv, rclcpp::Time(left->header.stamp).seconds());
 
   const rclcpp::Time tp_2 = node_->now();
+  TP_COMPUTE_CPU(
+      node_, std::chrono::nanoseconds(tp_2.nanoseconds() - tp_1.nanoseconds()),
+      std::string(__func__) + ":feed_stereo_frame");
   const double track_time = (tp_2 - tp_1).seconds();
 
   // track times in seconds
@@ -44,6 +57,8 @@ void stereo::callback(const sensor_msgs::msg::Image::ConstSharedPtr& left,
   if (cam_pose_wc) {
     publish_pose(*cam_pose_wc, left->header.stamp);
   }
+
+  TP_CALLBACK_ENTER(node_, node_->now(), __func__);
 }
 
 }  // namespace openvslam_ros
